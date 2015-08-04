86355613401b3113c136745f859c7b013cb813be
Sukhmanbir <sukhmanbir.kaur@outlook.com>
2015-08-04 00:57:17 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,15 @@
-<footer class="alert alert-info navbar-fixed-bottom">
-	&copy;2015 - SUPER CARS <?php echo Date('M-d-Y')
-?>
+</div>
+<!-- End of Container -->
+<footer class="navbar-fixed-bottom">
+	<p>
+		<strong>&copy;<?php echo Date('Y'); ?>
+		- CARS - All Rights Reserved.</strong>
+	</p>
 </footer>
+
+<!-- Place Javascript in the footer so that the page loads faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
 </body>
 
